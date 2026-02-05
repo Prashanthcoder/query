541a73cd8e3e4d9a3dd6299bac4185973694cda0
Prashanthcoder <tp120113@gmail.com>
2026-02-05 22:57:33 +0530

    Added authentication loading task using multithreaded delay

--- Sol.java
+++ Sol.java
@@ -8,6 +8,14 @@ public class Sol {
         System.out.println("Enter the password : ");
         String password = sc.nextLine();
         if ((username.equals("Prashanthcoder")) && (password.equals("Prashi@9098"))) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    Thread.sleep(500); // Wait 1 second
+                    System.out.print(".");
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Error during delay.");
+            }
             System.out.println("LOGGED IN\n");
             for (int i = 0; i < 20; i++) {
                 System.out.println("_________________WELCOME TO THE PRASHANTH TECH LTD___________________________");
